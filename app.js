2754d47a9f1c949143763fe820046d60c31682b4
Johan Eliasson <jeliasson@users.noreply.github.com>
2017-07-23 02:11:41 +0200

    0.2.0

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-// Requires
+const VERSION = '0.2.0';
+
 var Discord = require('discord.js');
 var isset = require('isset');
+var empty = require('empty');
 var md5 = require('md5');
 var rs = require('random-string');
 var uptimer = require('uptimer');
 var checksum = require('checksum');
 var path = require('path');
 
-// Application
 var App = {
-    version: '0.1.0',
+
+    // Discord
+    Discord: {
+        token: '',
+        presence: 'God',
+        client: {},
+    },
+
+    // Dynamic Channel Name
+    DynamicChannelName: {
+        enabled: true,                          // Enable keep-alive console log
+        channelPrefix: '~ ',                    // Only process channels with this prefix
+        defaultChannelName: 'Room',             // Default channel name
+        defaultChannelNameEmpty: 'Room',        // Default empty channel name
+        minPresenceDominanceProcentage: 50,     // Minimum procentage condition before changing channel name
+        minParticipant: 0,                      // Minimum of participant in a channel before changing channel name,
+        maxChannelSpawn: 10,                    // @todo
+    },
+
+    // Logger
+    logger: {
+        enabled: true,
+        service: {
+            discord: {
+                enabled: false,                 // Log to Discord
+                channel: 'bot-log'              // Log to text channel name
+            }
+        }
+    },
+
+    // Instance
     instance: {
         keepalive: {
             enabled: true,
@@ -17,22 +48,7 @@ var App = {
         }
     },
 
-    token: '',
-
-    // Discord
-    Discord: {
-        client: {},
-    },
-
-    // Dynamic Channel Name
-    DynamicChannelName: {
-        enabled: true,
-        channelPrefix: '~ ',                    // Only process channels with this prefix
-        defaultChannelName: 'Room',             // Default channel name
-        defaultChannelNameEmpty: 'Room',        // Default empty channel name
-        minPresenceDominanceProcentage: 51,     // Minimum procentage condition before changing channel name
-        minParticipant: 0,                       // Minimum of participant in a channel before changing channel name
-    },
+    version: VERSION,
 
     // Run
     run: function(options, callback) {
@@ -44,14 +60,14 @@ var App = {
         App.Discord.client = new Discord.Client();
 
         // Login using token
-        App.Discord.client.login(App.token);
+        App.Discord.client.login(App.Discord.token);
 
         // Client ready
         App.Discord.client.on('ready', function() {
-            console.log('** Logged in as ' + App.Discord.client.user.tag);
+            App.log('** Discord is ready (' + App.Discord.client.user.tag + ')');
 
             // Set App.Discord.client information
-            App.Discord.client.user.setGame('God');
+            App.Discord.client.user.setGame(App.Discord.presence);
 
             // Run callback
             if (typeof callback == "function") {
@@ -64,15 +80,15 @@ var App = {
          */
         App.Discord.client.on('presenceUpdate', function(statusBefore, statusAfter) {
 
-            App.handleDynamicChannelName(App.Discord.client, statusAfter);
+            App.channelProcessing(App.Discord.client, statusAfter);
         });
 
         /*
-         * Voice
+         * Voice Event
          */
          App.Discord.client.on('voiceStateUpdate', function(VoiceChannel, User) {
 
-             App.handleDynamicChannelName(App.Discord.client, VoiceChannel); // Emitted everytime voice state change
+             App.channelProcessing(App.Discord.client, VoiceChannel); // Emitted everytime voice state change
          });
 
         /*
@@ -86,10 +102,22 @@ var App = {
     },
 
     setup: function() {
-        // Only run once
-        if (isset(App.instance.setupCompleted)) return;
 
-        console.info('** Starting instance');
+        // Only run once
+        if (isset(App.Discord.setupCompleted)) return;
+
+        // Application logger
+        App.log = function (data = null, options) {
+            console.log(data);
+
+            // Discord logging
+            if (!isset(App.Discord.channelProcessingisReady) || !App.logger.service.discord.enabled) return;
+            App.logger.service.discord.channelObject.sendMessage(data, {
+
+            });
+        };
+
+        App.log('** Starting instance');
 
         // Application file name
         App.instance.file = path.basename(__filename);
@@ -99,11 +127,10 @@ var App = {
 
         // Application checksum
         App.instance.checksum = checksum.file(path.basename(__filename), function(err, checksum) {
-            if (err) console.warn(err);
-
+            if (err) App.log(err);
             App.instance.checksum = checksum;
 
-            console.info('** Instance id ' + App.instance.id + ' on checksum ' + App.instance.checksum);
+            App.log('** Instance id ' + App.instance.id + ' on checksum ' + App.instance.checksum);
         });
 
         // Application uptime
@@ -117,16 +144,18 @@ var App = {
         setInterval(function() {
             if (App.instance.keepalive.enabled) {
                 App.instance.keepalive.count++;
-                console.info('** Keep-Alive ' + App.instance.keepalive.count + '/' + App.instance.keepalive.interval);
+                App.log('** Keep-Alive ' + App.instance.keepalive.count + '/' + App.instance.keepalive.interval);
             }
-        }, App.instance.keepalive.interval * 1000);
+        }, (App.instance.keepalive.interval * 1000));
 
         App.instance.setupCompleted = true;
     },
 
     // Handle Dynamic Channel Name
-    handleDynamicChannelName: function(client, Channel) {
+    channelProcessing: function(client, Channel) {
         // Process all channels in the guild
+        var presenceTotals = [];
+        var channelsTotals = 0;
         App.Discord.client.channels.forEach(function(channel) {
             var presenceStats = [];
             presenceStats['.none'] = 0;
@@ -136,8 +165,9 @@ var App = {
 
                 // Match channel against channel prefix (filter)
                 if (channel.name.startsWith(App.DynamicChannelName.channelPrefix)) {
-                    console.log('========================================================');
-                    console.log('[' + channel.id + '] DCN: Processing channel ' + channel.name + '...');
+
+                    channelsTotals++;
+                    App.log('\nDCN: Processing channel ' + channel.name + '...');
 
                     // Loop thru all channel participants for this channel
                     var channelParticipants = 0;
@@ -172,17 +202,18 @@ var App = {
                     }
                     presence = presenceStatsHighest;
 
-                    // Construct channel name suggestion
-                    var suggestion;
-                    suggestion = App.DynamicChannelName.channelPrefix;
+                    // Construct channel name suggestion.channelName
+                    var suggestion = {};
+                    suggestion.channelPrefix = App.DynamicChannelName.channelPrefix;
+                    suggestion.channelName = '';
+                    suggestion.channelNumber = '';
 
                     // 0 channel participants (don't know if this will ever run).
                     if (channelParticipants === 0) {
-                        suggestion += App.DynamicChannelName.defaultChannelNameEmpty;
-                        suggestion += ' #' + channel.id.substring(channel.id.length-3, channel.id.length);
+                        suggestion.channelName = App.DynamicChannelName.defaultChannelNameEmpty;
 
-                    // If presence is meeting target procentage, handle 2 participants.
                     } else if (
+                        // If presence is meeting target procentage, handle 2 participants.
                         (presenceProcentage >= App.DynamicChannelName.minPresenceDominanceProcentage)
                         ||
                         (presenceProcentage >= App.DynamicChannelName.minPresenceDominanceProcentage-1 && channelParticipants >= 2)
@@ -191,39 +222,82 @@ var App = {
                         if (presence == '.none') presence = App.DynamicChannelName.defaultChannelName;
                         if (presence == 'iexplore') presence = 'Silly Goose!';
 
-                        suggestion += presence;
-                        suggestion += ' #' + channel.id.substring(channel.id.length-3, channel.id.length);
+                        suggestion.channelName = presence;
 
-                    // Zero presene
                     } else if (presence == '.none') {
-                        suggestion += App.DynamicChannelName.defaultChannelName;
-                        suggestion += ' #' + channel.id.substring(channel.id.length-3, channel.id.length);
-
-                    // Else
+                        // Zero presene
+                        suggestion.channelName = App.DynamicChannelName.defaultChannelName;
                     } else {
                         // Do nothing
+                        suggestion.channelName = App.DynamicChannelName.defaultChannelName;
                     }
 
-                    if (App.DynamicChannelName.enabled && channel.name != suggestion) {
-                        console.log('[' + channel.id + '] DCN: Changing channel name to \'' + suggestion + '\'');
-                        channel.setName(suggestion);
-                    } else if (App.DynamicChannelName.enabled && channel.name == suggestion) {
-                        console.log('[' + channel.id + '] DCN: Channel name is already per suggestion: ' + suggestion + '\'');
-                        //channel.setName(suggestion);
+                    nameGenerator = true;
+                    nameGeneratorCount = 0;
+                    while (nameGenerator) {
+                        nameGeneratorCount++;
+
+                        // Channel Number
+                        if (nameGeneratorCount > 1) {
+                            suggestion.channelNumber = ((nameGeneratorCount < 10) ? ("0" + nameGeneratorCount) : nameGeneratorCount);
+                        }
+
+                        // Default channel room
+                        suggestion.channelNumber = '(Room ' + channelsTotals + ')';
+
+                        // Override defualt channel rooms
+                        if (suggestion.channelName == App.DynamicChannelName.defaultChannelName) {
+                            suggestion.channelNumber = channelsTotals;
+                        }
+
+                        // Final suggestion to evaluate
+                        suggestion.final = suggestion.channelPrefix + ' ' + suggestion.channelName + ' ' + suggestion.channelNumber;
+
+                        if (presenceTotals.indexOf(suggestion.final) <= -1) {
+                            presenceTotals.push(suggestion.final);
+                            nameGenerator = false;
+
+                            break;
+                        }
+
+                        // Break out if we have tried more than max channel spawn count
+                        if (nameGeneratorCount > App.DynamicChannelName.maxChannelSpawn) break;
+                    }
+
+                    if (App.DynamicChannelName.enabled && channel.name != suggestion.final) {
+                        App.log('DCN: Changing channel name to \'' + suggestion.final + '\'');
+                        channel.setName(suggestion.final);
+                    } else if (App.DynamicChannelName.enabled && channel.name == suggestion.final) {
+                        App.log('DCN: Channel name is already per suggestion: ' + suggestion.final + '\'');
                     } else {
-                        console.log('[' + channel.id + '] DCN: Suggesting changing channel name to \'' + suggestion + '\'');
+                        App.log('DCN: Suggesting changing channel name to \'' + suggestion.final + '\'');
                     }
 
                 }
 
             }
+
+            // Voice Channels
+            if (channel.type == 'text') {
+
+                // Match and save Discord logging channel
+                if (channel.name.startsWith(App.logger.service.discord.channel)) {
+                    App.logger.service.discord.channelObject = channel;
+                }
+            }
         });
+
+        App.Discord.channelProcessingisReady = true;
     },
 
     // Handle Messages
     handleMessage: function(client, message) {
-        console.log('==[ MESSAGE ]=================');
-        console.log('<= ' + message.author.username + ': ' + message.content);
+
+        // Block messages from log channel
+        if (message.channel.name == App.logger.service.discord.channel) return;
+
+        // Log inlogging message
+        App.log('[MESSAGE] ' + message.author.username + ' in ' + message.channel.name + ': ' + message.content);
 
         // Ping, Pong!
         if (message.content === 'help') {
@@ -248,7 +322,7 @@ var App = {
 
         // Debug
         if (message.content === 'debug') {
-            message.reply('[' + App.instance.id + '] ' + ' Debug Information\n========================================\n\n# App\nVersion: ' + App.version + '\n\n# Instance\nId: ' + App.instance.id + '\nChecksum: ' + App.instance.checksum + '\nFile: ' + App.instance.file + '\nUptime = ' + App.instance.uptime + '\nKeep-Alive Count: ' + App.instance.keepalive.count + '\nKeep-Alive Interval: ' + App.instance.keepalive.interval + '\n\n# Processes Environment\nComputer Name: ' + process.env.COMPUTERNAME + '\n\n========================================');
+            message.reply('[' + App.instance.id + '] ' + ' Debug Information\n========================================\n\n:: App\nVersion: ' + App.version + '\n\n:: Instance\nId: ' + App.instance.id + '\nFile: ' + App.instance.file + '\nChecksum: ' + App.instance.checksum + '\nUptime = ' + App.instance.uptime + '\nKeep-Alive Count: ' + App.instance.keepalive.count + '\nKeep-Alive Interval: ' + App.instance.keepalive.interval + '\n\n:: Environment\nComputer Name: ' + process.env.COMPUTERNAME + '\n\n========================================');
 
             return;
         }
